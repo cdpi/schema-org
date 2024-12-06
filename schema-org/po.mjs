eb769ca40577409095f2d64f4de88caf2b1d55b3
Fabrice Theytaz <2198753+fabricetheytaz@users.noreply.github.com>
2024-12-06 16:35:42 +0100

    Rien

--- schema-org/po.mjs
+++ schema-org/po.mjs
@@ -2,7 +2,6 @@
 import { writeFileSync } from "node:fs";
 import { parseProperties } from "./parser/csv/parse.mjs";
 
-// Thing ou Property
 const label = object => object.label;
 
 const msg = label => `msgid "${label}"
@@ -10,19 +9,6 @@ msgstr ""
 
 `;
 
-/*
-const msg = property => `msgid "${property.label}.label"
-msgstr "${property.label}"
-
-msgid "${property.label}.comment"
-msgstr "${property.comment.replaceAll("\"", "\\\"")}"
-
-`;
-*/
-
 const po = path => parseProperties(path).map(label).map(msg).join("");
-
-//let properties = parseProperties("parser/csv/schemaorg-all-https-properties.csv").map(label).map(msg);
-//writeFileSync("fr.po", properties.join(""));
 
 writeFileSync("properties.po", po("parser/csv/schemaorg-all-https-properties.csv"));
